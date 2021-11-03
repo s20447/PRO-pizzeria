d49bce4d0895484ab88284c0423b2268c428986e
Hubert Opatczyk <s20447@pjwstk.edu.pl>
2021-11-03 19:31:51 +0100

    Initial commit

---
+++ PRO Pizzeria API test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRO_Pizzeria_API_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
